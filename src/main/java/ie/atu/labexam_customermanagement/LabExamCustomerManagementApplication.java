63ad021a2fe80ce2dacb1af48f317706d9813d80
oskark22 <oskar.koskiranta@gmail.com>
2024-10-21 12:13:28 +0100

    Started project

---
+++ src/main/java/ie/atu/labexam_customermanagement/LabExamCustomerManagementApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.labexam_customermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabExamCustomerManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabExamCustomerManagementApplication.class, args);
+    }
+
+}
